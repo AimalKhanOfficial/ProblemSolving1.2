2841f0aee7d49c5ef9fd5c51dd27f7bd76005f85
aimalkhanofficial <aimalkhanofficial@gmail.com>
2020-01-02 23:32:36 -0500

    Really, really basic inorder traversal

--- Data Structures/Trees/Index.js
+++ Data Structures/Trees/Index.js
@@ -16,8 +16,20 @@ var increasingBST = function(root) {
 };
 
 let simpleInOrder = (root) => {
-    console.log(root);
+    //base condition
+    if (root === undefined || root === null) {
+        return;
+    }
+    if (root.left === undefined || root.left === null && root.right === undefined || root.right === null) {
+        console.log(root.val);
+        return;
+    }
+    simpleInOrder(root.left);
+    console.log(root.val);
+    simpleInOrder(root.right);
 }
+
+//tree1 
 
 let root = new TreeNode(5);
 root.left = new TreeNode(3); 
@@ -31,4 +43,11 @@ root.right.right = new TreeNode(8);
 root.right.right.right = new TreeNode(9);
 root.right.right.left = new TreeNode(7);
 
-simpleInOrder(root);
+//tree2
+let root2 = new TreeNode(1);
+root2.left = new TreeNode(2);
+root2.right = new TreeNode(3);
+root2.left.left = new TreeNode(4);
+root2.left.right = new TreeNode(5);
+
+simpleInOrder(root2);
