394fa0caa0d12f03d25531842fa7345f6e9ccdee
aimalkhanofficial <aimalkhanofficial@gmail.com>
2020-02-10 07:33:40 -0500

    Find Distict Pairs

---
+++ Other problems/FindPairs.js
@@ -0,0 +1,18 @@
+//Brute force solution
+
+const isFirstPointerValLess = (a, b) => a < b;
+const findDistinchPairs = (arr=[]) => {
+    let hashMap = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) continue;
+            if (Math.abs(arr[i]-arr[j])===2) {
+                isFirstPointerValLess(arr[i], arr[j]) ? hashMap.set(arr[i], arr[j]) : hashMap.set(arr[j], arr[i]);
+            }
+        }
+    }
+    return hashMap;
+}
+
+const res = findDistinchPairs([1, 7, 5, 9, 2, 12, 3]);
+console.log(res);
